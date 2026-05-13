626c5f85d92017f0d6ad5a53d2fc3ddef9e3a135
sigma700 <196065961+sigma700@users.noreply.github.com>
2026-05-14 02:50:39 +0300

    fixed cors issue

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://dk-com-frontend.onrender.com/",
+    origin: "https://dk-com-frontend.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   }),
