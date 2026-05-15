98be45377a1ef6ff04fb4ae9bf1060f1ca2e69f5
sigma700 <196065961+sigma700@users.noreply.github.com>
2026-05-15 22:20:41 +0300

    fixed url issue

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://mindfullivingke.onrender.com/",
+    origin: "https://mindfullivingke.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   }),
