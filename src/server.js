57d680ed794e4ae2aab9132044c585aff4b0d111
sigma700 <196065961+sigma700@users.noreply.github.com>
2026-05-15 21:37:42 +0300

    fixed final part

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "/",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   }),
